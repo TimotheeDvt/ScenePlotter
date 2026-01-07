6c3e583c4dd63a56e1a05aee8f98c406d7556e32
TimotheeDvt <timothee.dravet@gmail.com>
2026-01-07 19:58:52 +0100

    Add SVG for main instruments and mic

--- svgs.js
+++ svgs.js
@@ -1,9 +1,53 @@
 const SVG_LIBRARY = [
+	// Instruments
+	{
+		"name": "Drums",
+		"path": "drums.svg",
+		"outputNbAnchors": 8,
+		"inputNbAnchors": 0,
+		"category": "Instruments"
+	},
 	{
 		"name": "Guitar",
 		"path": "guitar.svg",
 		"outputNbAnchors": 1,
 		"inputNbAnchors": 0,
-		"category": "Guitars"
+		"category": "Instruments"
+	},
+	{
+		"name": "EGuitar",
+		"path": "electric-guitar.svg",
+		"outputNbAnchors": 1,
+		"inputNbAnchors": 0,
+		"category": "Instruments"
+	},
+	{
+		"name": "Bass",
+		"path": "bass.svg",
+		"outputNbAnchors": 1,
+		"inputNbAnchors": 0,
+		"category": "Instruments"
+	},
+	{
+		"name": "Trumpet",
+		"path": "trumpet.svg",
+		"outputNbAnchors": 1,
+		"inputNbAnchors": 0,
+		"category": "Instruments"
+	},
+	{
+		"name": "Violin",
+		"path": "violin.svg",
+		"outputNbAnchors": 1,
+		"inputNbAnchors": 0,
+		"category": "Instruments"
+	},
+	// Microphones
+	{
+		"name": "SM58",
+		"path": "microphone.svg",
+		"outputNbAnchors": 1,
+		"inputNbAnchors": 0,
+		"category": "Microphones"
 	},
 ];
